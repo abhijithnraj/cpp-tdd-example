c2e11ee7584715eec66891f208d102d4f70c5bb1
Abhijith N Raj <abhijithnrajg@gmail.com>
2022-11-01 16:46:18 +0530

    Added a fixture class to remove duplicate initialisation code from tests

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -13,14 +13,17 @@ using ::testing::Eq; // for better readability
         }
 };
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+class SoundexEncoding : public ::testing::Test {
+  public: 
   Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
   auto encoded = soundex.encode("A");
   ASSERT_THAT(encoded, Eq("A000"));
 }
 
-TEST(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits){
-  Soundex soundex;
+TEST_F(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits){
   auto encoded = soundex.encode("I");
   ASSERT_THAT(encoded,Eq("I000"));
 }
