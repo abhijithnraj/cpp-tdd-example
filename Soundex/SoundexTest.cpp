c7f73fc025b94cce65e24ab5290f8d20167d9cc0
Abhijith N Raj <abhijithnrajg@gmail.com>
2022-11-01 15:44:08 +0530

    RetainsSoleLetterOfOneLetterWord passing for hardcoded A

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -2,14 +2,14 @@
  class Soundex {
      public:
         std::string encode(const std::string& word) const {
-            return "";
+            return "A";
          }
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
-Soundex soundex;
-auto encoded = soundex.encode("A");
-ASSERT_THAT(encoded, testing::Eq("A"));
+  Soundex soundex;
+  auto encoded = soundex.encode("A");
+  ASSERT_THAT(encoded, testing::Eq("A"));
 }
 
 
