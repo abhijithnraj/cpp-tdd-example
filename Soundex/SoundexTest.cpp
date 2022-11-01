bebd0355539b74cdacf236d2e0818a904333999a
Abhijith N Raj <abhijithnrajg@gmail.com>
2022-11-01 16:38:10 +0530

    Added More code to ensure zero padding requirement

--- Soundex/SoundexTest.cpp
+++ Soundex/SoundexTest.cpp
@@ -5,17 +5,21 @@ using ::testing::Eq; // for better readability
  class Soundex {
      public:
         std::string encode(const std::string& word) const {
-            return "A";
+            return word+"000";
          }
 };
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
   Soundex soundex;
   auto encoded = soundex.encode("A");
-  ASSERT_THAT(encoded, Eq("A"));
+  ASSERT_THAT(encoded, Eq("A000"));
 }
 
-
+TEST(SoundexEncoding, PadsWithZeroesToEnsureThreeDigits){
+  Soundex soundex;
+  auto encoded = soundex.encode("I");
+  ASSERT_THAT(encoded,Eq("I000"));
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
